ab0d05cb1a0ac01099be1566802eead5d53f6a4d
DevFrog <froggi@xs4all.nl>
2024-02-19 23:20:37 +0100

    Code cleanup and exported  determine_light_params to helpe file

--- lights_updater.py
+++ lights_updater.py
@@ -1,52 +1,20 @@
 from pathlib import Path
 from helpers import make_backup
+from helpers import determine_light_params
 
 filepath = "/media/froggi/Flightsim/X-Plane 12/Resources/plugins/LiveTraffic/Resources/CSL/BB_Boeing/B773"
 # Old params
-NEEDLES = ("LIGHT_NAMED airplane_landing", "LIGHT_SPILL_CUSTOM")
-NEW_FILE_EXTENSION = ".obj.NEW"
-LIGHT_JETS = ("B733", "B734", "B737", "B738", "B739", "A318", "A319", "A320", "A321")
-HEAVY_JETS = (
-    "B717",
-    "B744",
-    "B74F",
-    "B752",
-    "B763",
-    "B772",
-    "B773",
-    "B77L",
-    "B77W",
-    "B788",
-    "A306",
-    "A310",
-    "A332",
-    "A333",
-    "A337",
-    "A342",
-    "A343",
-    "A345",
-    "A346",
-    "A359",
-    "A388",
+NEEDLES = (
+    "LIGHT_NAMED airplane_landing",
+    "LIGHT_SPILL_CUSTOM",
+    "LIGHT_NAMED airplane_taxi",
+    "LIGHT_NAMED airplane_nav_left",
+    "LIGHT_NAMED airplane_nav_right",
+    "LIGHT_NAMED airplane_nav_tail",
 )
+NEW_FILE_EXTENSION = ".obj.NEW"
 
 # TODO: Navlights, Strobes, Beacon, Taxi
-
-
-# temporary new light parameters
-def determine_light_params(aircraft_type: str) -> str:
-    """Determines the paramaters for the give aircraft type
-
-    Args:
-        aircraft_type (str): Aircraft type e.g. B733 for Boeing 737-300
-
-    Returns:
-        str: A string with the corresponding light parameters for the given aircraft type
-    """
-    if aircraft_type in HEAVY_JETS:
-        return "0.76052475 0.65837479 0.57758057 3 765000cd 0.034766696 -0.052357007 -0.99802303 0.97992471"
-    if aircraft_type in LIGHT_JETS:
-        return "0.76052475 0.65837479 0.57758057 3 200000cd 0.034766696 -0.052357007 -0.99802303 0.97992471"
 
 
 def process_line(line: str, xp12_params: str) -> str:
@@ -60,36 +28,31 @@ def process_line(line: str, xp12_params: str) -> str:
         str: _description_
     """
 
-    # New
+    # New light params
     new_landing_light_params = "LIGHT_PARAM airplane_landing_pm"
     new_landing_light_billboard_params = "LIGHT_PARAM airplane_landing_bb"
+    xyz_light_coordinates = " ".join(line.split()[:3])
 
     if line.startswith(NEEDLES[0]):
         print("##################### SPILL-LIGHT ##################")
         print("Spill:", line)
-        new_spill_line = line.replace(
+        new_spill_line = xyz_light_coordinates.replace(
             NEEDLES[0], new_landing_light_billboard_params
         ).replace("\n", "")
         new_spill_line += f" {xp12_params}\n"
         print("New Spill:", new_spill_line)
         print("################### END SPILL-LIGHT ################")
         return new_spill_line
-    if line.startswith(NEEDLES[1]):  # TODO: new syntax
+    if line.startswith(NEEDLES[1]):
         print("--------------------- LANDING-LIGHTS -----------------")
         print("Old landinglights:", line.replace("\n", "").split(" "))
-        # try:
-        #     [light_param, light_name, lat, long, height] = line.replace("\n", "").split(
-        #         " "
-        #     )
-        # except ValueError as err:
-        #     print(err)
-
-        new_line = line.replace(NEEDLES[1], new_landing_light_params).replace("\n", "")
+        new_line = xyz_light_coordinates.replace(
+            NEEDLES[1], new_landing_light_params
+        ).replace("\n", "")
         new_line += f" {xp12_params}\n"
         print("New landinglights:", new_line)
         print("------------------------------------------------------")
         return new_line
-    # return line
 
 
 def get_object_files(filepath: str) -> list:
