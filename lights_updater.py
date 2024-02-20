b2c26a41f18a488edf95f8a79b52e2be828f6138
DevFrog <froggi@xs4all.nl>
2024-02-20 02:26:34 +0100

    Some changes to test locally...

--- lights_updater.py
+++ lights_updater.py
@@ -2,19 +2,20 @@ from pathlib import Path
 from helpers import make_backup
 from helpers import determine_light_params
 
-filepath = "/media/froggi/Flightsim/X-Plane 12/Resources/plugins/LiveTraffic/Resources/CSL/BB_Boeing/B773"
+filepath = "./CSL/BB_Boeing/B773"
 # Old params
-NEEDLES = (
-    "LIGHT_NAMED airplane_landing",
-    "LIGHT_SPILL_CUSTOM",
-    "LIGHT_NAMED airplane_taxi",
-    "LIGHT_NAMED airplane_nav_left",
-    "LIGHT_NAMED airplane_nav_right",
-    "LIGHT_NAMED airplane_nav_tail",
+LIGHT_NEEDLES = (
+    "airplane_landing",
+    "airplane_taxi",
+    "airplane_nav_left",
+    "airplane_nav_right",
+    "airplane_nav_tail",
+    "airplane_strobe",
+    "airplane_beacon",
 )
+SPILL_NEEDLE = "LIGHT_SPILL_CUSTOM"
+LIGHT_REPLACE = {"old": "LIGHT_NAMED", "new": "LIGHT_PARAM"}
 NEW_FILE_EXTENSION = ".obj.NEW"
-
-# TODO: Navlights, Strobes, Beacon, Taxi
 
 
 def process_line(line: str, xp12_params: str) -> str:
@@ -28,31 +29,50 @@ def process_line(line: str, xp12_params: str) -> str:
         str: _description_
     """
 
+    # Replace OLD with NEW lightparam name
+    linestart = (
+        " ".join(line.split()[:1])
+        .replace(LIGHT_REPLACE["old"], LIGHT_REPLACE["new"])
+        .replace(SPILL_NEEDLE, LIGHT_REPLACE["new"])
+    )
+
+    line = line.replace(LIGHT_REPLACE["old"], "")
+
+    light_type = " ".join(line.split()[:1])
+
+    # line = line.replace(light_type, "")
+
     # New light params
-    new_landing_light_params = "LIGHT_PARAM airplane_landing_pm"
-    new_landing_light_billboard_params = "LIGHT_PARAM airplane_landing_bb"
+    new_light_suffix = "_pm"
+    new__billboard_suffix = "_bb"
+
     xyz_light_coordinates = " ".join(line.split()[:3])
 
-    if line.startswith(NEEDLES[0]):
+    # Check if line starts with SPILL_NEEDLE
+    if line.startswith(SPILL_NEEDLE):
         print("##################### SPILL-LIGHT ##################")
         print("Spill:", line)
-        new_spill_line = xyz_light_coordinates.replace(
-            NEEDLES[0], new_landing_light_billboard_params
+        new_spill_line = line.replace(
+            SPILL_NEEDLE,
         ).replace("\n", "")
         new_spill_line += f" {xp12_params}\n"
         print("New Spill:", new_spill_line)
         print("################### END SPILL-LIGHT ################")
-        return new_spill_line
-    if line.startswith(NEEDLES[1]):
+        print(f"{linestart} {light_type}_bb {new_spill_line}")
+        return f"{linestart} {light_type}_bb {new_spill_line}"
+
+    # Check if linestart is in LIGHT_NEEDLES tuple
+    if " ".join(line.split()[:1]) in LIGHT_NEEDLES:
         print("--------------------- LANDING-LIGHTS -----------------")
         print("Old landinglights:", line.replace("\n", "").split(" "))
         new_line = xyz_light_coordinates.replace(
-            NEEDLES[1], new_landing_light_params
+            LIGHT_REPLACE["old"], new_landing_light_params
         ).replace("\n", "")
         new_line += f" {xp12_params}\n"
         print("New landinglights:", new_line)
         print("------------------------------------------------------")
-        return new_line
+        print(f"{linestart} {light_type}_pm {new_line}")
+        return f"{linestart} {light_type}_pm {new_line}"
 
 
 def get_object_files(filepath: str) -> list:
@@ -84,7 +104,9 @@ def create_new_object_file(aircraft_object_file: Path) -> None:
 
         for line in aircraft_object:
             with open(new_file, "a") as new_obj_file:
-                if line.startswith(NEEDLES[0]) or line.startswith(NEEDLES[1]):
+                if line.startswith(LIGHT_REPLACE["old"]) or line.startswith(
+                    SPILL_NEEDLE
+                ):
                     newline = process_line(line, xp12_params)
                     new_obj_file.write(newline)
                     continue
