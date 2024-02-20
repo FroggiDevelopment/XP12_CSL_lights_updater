ff2fdb0112b123ae73616f40466da0c41c075c6e
DevFrog <froggi@xs4all.nl>
2024-02-20 03:36:51 +0100

    Added obj file(s) for test Changed logic. To be continued...

--- lights_updater.py
+++ lights_updater.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from helpers import make_backup
 from helpers import determine_light_params
 
-filepath = "./CSL/BB_Boeing/B773"
+filepath = "./CSL"
 # Old params
 LIGHT_NEEDLES = (
     "airplane_landing",
@@ -18,63 +18,6 @@ LIGHT_REPLACE = {"old": "LIGHT_NAMED", "new": "LIGHT_PARAM"}
 NEW_FILE_EXTENSION = ".obj.NEW"
 
 
-def process_line(line: str, xp12_params: str) -> str:
-    """Process line to look for landing lights or spill entry and replace with new params
-
-    Args:
-        line (str): line from aircarft object file
-        xp12_params (str): _description_
-
-    Returns:
-        str: _description_
-    """
-
-    # Replace OLD with NEW lightparam name
-    linestart = (
-        " ".join(line.split()[:1])
-        .replace(LIGHT_REPLACE["old"], LIGHT_REPLACE["new"])
-        .replace(SPILL_NEEDLE, LIGHT_REPLACE["new"])
-    )
-
-    line = line.replace(LIGHT_REPLACE["old"], "")
-
-    light_type = " ".join(line.split()[:1])
-
-    # line = line.replace(light_type, "")
-
-    # New light params
-    new_light_suffix = "_pm"
-    new__billboard_suffix = "_bb"
-
-    xyz_light_coordinates = " ".join(line.split()[:3])
-
-    # Check if line starts with SPILL_NEEDLE
-    if line.startswith(SPILL_NEEDLE):
-        print("##################### SPILL-LIGHT ##################")
-        print("Spill:", line)
-        new_spill_line = line.replace(
-            SPILL_NEEDLE,
-        ).replace("\n", "")
-        new_spill_line += f" {xp12_params}\n"
-        print("New Spill:", new_spill_line)
-        print("################### END SPILL-LIGHT ################")
-        print(f"{linestart} {light_type}_bb {new_spill_line}")
-        return f"{linestart} {light_type}_bb {new_spill_line}"
-
-    # Check if linestart is in LIGHT_NEEDLES tuple
-    if " ".join(line.split()[:1]) in LIGHT_NEEDLES:
-        print("--------------------- LANDING-LIGHTS -----------------")
-        print("Old landinglights:", line.replace("\n", "").split(" "))
-        new_line = xyz_light_coordinates.replace(
-            LIGHT_REPLACE["old"], new_landing_light_params
-        ).replace("\n", "")
-        new_line += f" {xp12_params}\n"
-        print("New landinglights:", new_line)
-        print("------------------------------------------------------")
-        print(f"{linestart} {light_type}_pm {new_line}")
-        return f"{linestart} {light_type}_pm {new_line}"
-
-
 def get_object_files(filepath: str) -> list:
     """Get all object files within filepath
 
@@ -87,35 +30,60 @@ def get_object_files(filepath: str) -> list:
     return list(Path(filepath).rglob("*.[oO][bB][jJ]"))
 
 
+def inspect(anim_block):
+    if any(light in anim_block for light in LIGHT_NEEDLES):
+        print(anim_block)
+        return anim_block
+
+
+def create_anim_block(aircraft_object: Path) -> str:
+    """Create the anime start until anim end block for investigation
+
+    Args:
+        aircraft_object (Path): path to aircraft obj file
+
+    Returns:
+        str: Contains the full block between ANIM_start and ANIM_end
+    """
+    anim_block = ""
+    copy = False
+    for line in aircraft_object:
+        if line.startswith("ANIM_begin"):
+            copy = True
+            anim_block = ""
+        elif line.startswith("ANIM_end"):
+            copy = False
+            if anim_block != "":
+                inspect(anim_block)
+        elif copy:
+            anim_block += line
+    return anim_block
+
+
 def create_new_object_file(aircraft_object_file: Path) -> None:
     """Create new aircraft obj file with X-Plane 12 light params
 
     Args:
         file (Path): the existing aircraft object file
     """
-    new_file = aircraft_object_file.with_suffix(NEW_FILE_EXTENSION)
 
-    if new_file.exists():
-        new_file.unlink()
+    new_obj_file = aircraft_object_file.with_suffix(".obj.NEW")
 
     with open(aircraft_object_file) as aircraft_object:
-        aircraft_type = str(aircraft_object_file.parents[0]).split("/")[-1]
-        xp12_params = determine_light_params(aircraft_type)
+        anim_block = create_anim_block(aircraft_object)
 
-        for line in aircraft_object:
-            with open(new_file, "a") as new_obj_file:
-                if line.startswith(LIGHT_REPLACE["old"]) or line.startswith(
-                    SPILL_NEEDLE
-                ):
-                    newline = process_line(line, xp12_params)
-                    new_obj_file.write(newline)
-                    continue
-                new_obj_file.write(line)
+        if new_obj_file.exists():
+            new_obj_file.unlink()
+            print(f"Deleted {new_obj_file}")
+
+        with open(new_obj_file, "a") as new_aircraft_object:
+            print(f"Writing to file: {new_aircraft_object}")
+            new_aircraft_object.write(anim_block)
 
 
 def main():
     for file in get_object_files(filepath):
-        make_backup(file, ".obj.backup")
+        make_backup(file, ".obj.BCK")
         create_new_object_file(file)
 
 
