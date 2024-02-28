9c0f251f395873caa750946b6bcab838e8c9fb05
DevFrog <froggi@xs4all.nl>
2024-02-28 23:52:42 +0100

    New config class

---
+++ config.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+
+
+class Config:
+    def __init__(self, config_file: str) -> None:
+        self.config_file = config_file
+        self.config = self.get_config()
+
+    def get_config(self) -> dict[str]:
+        _config = {}
+        config_object = ConfigParser()
+        config_object.read(self.config_file)
+
+        return config_object._sections
+
+
+if __name__ == "__main__":
+    config = Config("./config.ini").get_config()
+    print(config)
