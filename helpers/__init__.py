227b977fc9dcb83271f1b705b0b1ecfed56adfc7
DevFrog <froggi@xs4all.nl>
2024-03-07 02:03:18 +0100

    Changed impprt of delete_files to delete_backups

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,4 @@
 from .helpers import make_backup
 from .helpers import recover_from_backup
-from .helpers import delete_files
+from .helpers import delete_backups
 from .aircraft_definitions import determine_light_params
