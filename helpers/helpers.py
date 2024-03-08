ecf6587f1763d01bee2ca962d30d5b330ba32dbe
DevFrog <froggi@xs4all.nl>
2024-03-08 01:30:56 +0100

    Added todo's fro errorhandling Changed level of logging were necessary

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -23,13 +23,13 @@ def make_backup(
                 backup_file.write_bytes(file.read_bytes())
             except PermissionError as err:
                 if stop_on_error == True:
-                    logging.debug("Stopping on error!", err)
+                    logging.error("Stopping on error!", err)
                     sys.exit()
             logging.debug(f"Backup for {file} is ready!")
             continue
-        logging.debug("Backup already exists for:", file)
+        logging.warning("Backup already exists for:", file)
     print("Backups done!")
-    debug.info("Backups done!")
+    logging.info("Backups done!")
 
 
 def delete_backups(files: list[Path], backup_extension: str) -> NoReturn:
@@ -37,6 +37,7 @@ def delete_backups(files: list[Path], backup_extension: str) -> NoReturn:
 
     Arguments: files: string
     """
+    # TODO: Error handling
     print("Start of deleting backups!")
     logging.info("Start of deleting backups!")
     delete_files(files, backup_extension)
@@ -58,16 +59,15 @@ def recover_from_backup(*, files: list, stop_on_error: bool = False) -> NoReturn
             recover_file.write_bytes(backup_file.read_bytes())
         except PermissionError as err:
             if stop_on_error == True:
-                logging.debug("Stopping on error!", err)
+                logging.error("Stopping on error!", err)
                 sys.exit()
             continue
         except FileNotFoundError as notfound:
             if stop_on_error == True:
-                logging.debug("Stopping because backup not found!", notfound)
+                logging.error("Stopping because backup not found!", notfound)
                 sys.exit()
             continue
         backup_file.unlink()
-        print(f"Recovery of {backup_file} is done!")
         logging.debug(f"Recovery of {recover_file} is done!")
     print("Recovery done!")
     logging.info("Recovery done!")
@@ -78,6 +78,7 @@ def delete_files(files: list[Path], suffix: str = None):
 
     Arguments: files: string List of files to be deleted.
     """
+    # TODO: Error handling
     for file in files:
         if suffix is not None:
             file = file.with_suffix(suffix)
