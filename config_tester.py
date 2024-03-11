adaae3f478f3a61d10f041ddfcc45ded5e4da25a
DevFrog <froggi@xs4all.nl>
2024-03-11 20:24:12 +0100

    Not in use anymore

--- config_tester.py
+++
@@ -1,8 +0,0 @@
-from configparser import ConfigParser
-from config import DynamicConfigIni
-
-parser = ConfigParser()
-parser.read_file(open("config.ini"))
-config = DynamicConfigIni(parser)
-
-print(config.generic.debug, config.csl.csl_path)
