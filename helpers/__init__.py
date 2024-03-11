96060b146177ace62a8e90e91f225ff18f83f4df
DevFrog <froggi@xs4all.nl>
2024-03-11 17:43:42 +0100

    Added new helper

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,3 +2,4 @@ from .helpers import make_backup
 from .helpers import recover_from_backup
 from .helpers import delete_backups
 from .aircraft_light_params import determine_light_params
+from .aircraft_helpers import get_aircraft_objects_from_xsb_file
