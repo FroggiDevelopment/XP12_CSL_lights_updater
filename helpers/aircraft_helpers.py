a7a970c3d9d6e780446eeffc2472ad3429323cd2
DevFrog <froggi@xs4all.nl>
2024-03-12 21:20:49 +0100

    Removed all occurances of is_xcsl as it is no longer used

--- helpers/aircraft_helpers.py
+++ helpers/aircraft_helpers.py
@@ -4,14 +4,11 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def get_aircraft_objects_from_xsb_file(
-    searchpath: str, is_xcsl: bool = False
-) -> list[str]:
+def get_aircraft_objects_from_xsb_file(searchpath: str) -> list[str]:
     """Get the aircraft objects from the xsb file and return the paths as a list.
 
     Args:
         searchpath (str): Topdirectory from which to search for the xsb_aircraft.txt file
-        is_xcsl (bool, optional): Is it a X-CSB package? Defaults to False.
 
     Returns:
         list[str]: List of paths to the aircraft objects in the searchpath.
@@ -23,10 +20,8 @@ def get_aircraft_objects_from_xsb_file(
     aircraft_desc_files: list[Path] = list(Path(searchpath).rglob("xsb_aircraft.txt"))
     aircraft_object_files: list[str] = []
 
-    # Separator differ between Bluebell and X-CSL
+    # Separator differ between Bluebell and X-CSL, standard is / in this case.
     _seperator: str = "/"
-    if is_xcsl == True:
-        _seperator = ":"
 
     # Start the search for the aircraft objects in the xsb_aircraft.txt file
     for desc_file in aircraft_desc_files:
@@ -38,15 +33,27 @@ def get_aircraft_objects_from_xsb_file(
                     continue
                 list_of_params: list[str] = line.split(" ")
                 aircraft_dir_file_info: str = list_of_params[3]
+
+                # Separator differ between Bluebell and X-CSL
+                if ":" in aircraft_dir_file_info:
+                    package = "xcsl"
+                    _seperator = ":"
+                elif "/" in aircraft_dir_file_info:
+                    package = "bluebell"
+                    _seperator: str = "/"
+
                 object_path = None
-                if len(list_of_params) > 5:  # X-CSL has texture info in this line!
+
+                # X-CSL has texture info in this line! So the number is greater than 5
+                if len(list_of_params) > 5:
                     object_file = aircraft_dir_file_info.split(_seperator)[1]
                     object_path = Path(parentdir, object_file)
-                elif is_xcsl == False:
+                elif package == "bluebell":
                     object_file = aircraft_dir_file_info.split(_seperator, 1)[1].rstrip(
                         "\n"
                     )
                     object_path = Path(parentdir, object_file)
+
                 if object_path is not None and object_path not in aircraft_object_files:
                     aircraft_object_files.append(object_path)
     return aircraft_object_files
