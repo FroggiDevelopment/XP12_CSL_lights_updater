686c3a53e687ee07ce1f7ec2f4e99659cb60f9da
DevFrog <froggi@xs4all.nl>
2024-03-15 13:48:42 +0100

    Moved list of aircrafts and list of lightparams to corresponding json files. Update aircraft_light_params.oy to read those json files for data

--- helpers/aircraft_light_params.py
+++ helpers/aircraft_light_params.py
@@ -15,314 +15,35 @@ Copyright (C) 2024  Richard J.M. Muller / Froggi
     along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
+import sys
 import json
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("aircraft_light_params")
 
-aircrafts: dict = {
-    "airliners": [
-        "B733",
-        "B733fa",
-        "B733ai",
-        "B734",
-        "B735",
-        "B735fa",
-        "B735wfa",
-        "B737",
-        "B738",
-        "B738s",
-        "B738WLai",
-        "B738WLfa",
-        "B738fa",
-        "B738ai",
-        "B739",
-        "B739WLfa",
-        "B739fa",
-        "B739s",
-        "A318",
-        "A318fCFM",
-        "A318dCFM",
-        "A319",
-        "A321fCFM",
-        "A321fIAE",
-        "A319fCFM",
-        "A319dCFM",
-        "A319dIAE",
-        "A319fIAE",
-        "A321wfCFM",
-        "A321wfIAE",
-        "A320fCFM",
-        "A320CFM",
-        "A320IAE",
-        "A320fIAE",
-        "A319fCFMw",
-        "A319fIAEw",
-        "A320wCFM",
-        "A320wIAE",
-        "A320",
-        "A321",
-        "B462",
-        "B463",
-        "CRJ2",
-        "CRJ7",
-        "CRJ9",
-        "CRJX",
-        "E135",
-        "E35L",
-        "E145",
-        "E145p1",
-        "E170",
-        "E175",
-        "E190",
-        "E195",
-        "F100",
-        "F28",
-        "F70",
-        "J328",
-        "MD81",
-        "MD83",
-        "MD90",
-        "A319f",
-        "A320w",
-        "A321w",
-        "AN72",
-        "AN74",
-        "IL62",
-        "SU95",
-        "TU134",
-        "TU154",
-        "TU204",
-        "T154",
-    ],
-    "heavy_airliners": [
-        "B717",
-        "B744",
-        "B744Fge",
-        "B744Fpw",
-        "B744Frr",
-        "B744rr",
-        "B74F",
-        "B744ge",
-        "B744pw",
-        "B748Ffa",
-        "B752",
-        "B752RR",
-        "B752PWold",
-        "B752RRold",
-        "B752PW",
-        "B753",
-        "B753RRold",
-        "B753PWold",
-        "B752PWwold",
-        "B752RRwold",
-        "B753PWw",
-        "B753RRw",
-        "B752PWw",
-        "B752RRw",
-        "B763",
-        "B763aiGEw",
-        "B763aiPWw",
-        "B763aiGE",
-        "B763aiPW",
-        "B763aiRR",
-        "B772",
-        "B773",
-        "B77L",
-        "B77F",
-        "B77W",
-        "B788",
-        "B789",
-        "A3ST",
-        "A306",
-        "A306PW",
-        "A310",
-        "A332",
-        "A332RRold",
-        "A332GEnew",
-        "A332RRnew",
-        "A332GEold",
-        "A332PWnew",
-        "A332PWold",
-        "A333",
-        "A333RR",
-        "A333GE",
-        "A333PW",
-        "A337",
-        "A342",
-        "A343",
-        "A345",
-        "A346",
-        "A359",
-        "A388",
-        "A388fuse",
-        "DC10",
-        "MD11",
-        "AN225",
-        "IL86",
-        "IL96",
-        "C17",
-        "C5M",
-        "C5",
-        "E3D",
-        "IL76",
-        "DC10",
-        "MD11",
-        "CONC",
-        "T144",
-        "B748fa",
-        "B772GE",
-        "B764aiGE",
-        "IL76a50",
-    ],
-    "props": [
-        "DH8D",
-        "AT42",
-        "AT72",
-        "AT75",
-        "D328",
-        "E120",
-        "F50",
-        "JS41",
-        "L410",
-        "SB20",
-        "SF34",
-        "AN12",
-        "AN22",
-        "AN24",
-        "AN26",
-        "AN28",
-        "AN30",
-        "AN32",
-        "AN8",
-        "C130",
-        "C130HL",
-        "C130HT",
-        "C130H",
-        "C30J",
-        "A400",
-    ],
-    "general_aviation": [
-        "BE20",
-        "C150",
-        "C172",
-        "C182",
-        "C421",
-        "G115",
-        "CL41",
-        "TUCA",
-        "PC9",
-        "SR22",
-        "BE58",
-        "C208",
-        "C208cp",
-        "PC12",
-        "SF50",
-    ],
-    "private_jets": [
-        "LJ45",
-        "H25B",
-        "C750",
-        "F900",
-        "F900W",
-    ],
-    "heli": [
-        "B06",
-        "EC35",
-        "EC75",
-        "S76",
-        "Mi26",
-        "AS350",
-    ],
-    "fighter_jets": [
-        "F16C",
-        "F16D",
-        "F18C",
-        "F18D",
-        "HAWK",
-        "M339",
-        "MG29",
-        "SU27",
-        "TOR",
-        "EUFI",
-        "F35",
-        "F22",
-    ],
-}
+try:
+    with open("aircrafts.json", "r") as aircrafts_definitions:
+        aircrafts = json.load(aircrafts_definitions)
+except FileNotFoundError:
+    logger.error("Missing aircrafts.json file. Stopping now!")
+    sys.exit(1)
+except json.decoder.JSONDecodeError:
+    logger.error(
+        "aircrafts.json may be corrupted. Please check the file for consistency!"
+    )
+    sys.exit(1)
 
-
-light_params = {
-    "heavy_airliners": {
-        "airplane_landing": "0.76052475 0.65837479 0.57758057 3 800000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 400000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 3000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 75000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-    "airliners": {
-        "airplane_landing": " 0.76052475 0.65837479 0.57758057 3 800000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 300000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 3000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 75000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-    "private_jets": {
-        "airplane_landing": " 0.76052475 0.65837479 0.57758057 3 600000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 100000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 3000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 75000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-    "fighter_jets": {
-        "airplane_landing": " 0.76052475 0.65837479 0.57758057 3 500000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 100000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 3000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 75000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-    "props": {
-        "airplane_landing": " 0.76052475 0.65837479 0.57758057 3 600000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 50000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 1000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 3000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 75000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-    "general_aviation": {
-        "airplane_landing": " 0.76052475 0.65837479 0.57758057 3 400000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 50000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 700cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 700cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 7000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 1500cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 5000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-    "heli": {
-        "airplane_landing": " 0.76052475 0.65837479 0.57758057 3 400000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_taxi": "0.76052475 0.65837479 0.57758057 3 50000cd 0.034766696 -0.052357007 -0.99802303 0.97992471",
-        "airplane_nav": "0.89626974 0.099898659 0 0 700cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_left": "0.89626974 0.099898659 0 0 700cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_right": "0.020288363 0.73791075 0.36130685 0 7000cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_nav_tail": "0.94730663 0.82278603 0.7230553 0 1500cd -0.077866882 -0.018656421 -0.99678928 0.34202015",
-        "airplane_strobe": "3.7129087 -0.07354179 -1.1133618 1 1 1 0 5000cd 0.66745675 0.06932088 0.74141496 0.4617486",
-        "airplane_beacon": "1 0 0 0 5000cd 0 0 0 1",
-    },
-}
+try:
+    with open("light_params.json", "r") as lights_definitions:
+        light_params = json.load(lights_definitions)
+except FileNotFoundError:
+    logger.error("Missing light_params.json file. Stopping now!")
+    sys.exit(1)
+except json.decoder.JSONDecodeError:
+    logger.error(
+        "light_params.json might be corrupted. Please check the file for consistency!"
+    )
+    sys.exit(1)
 
 
 def get_light_params_for_aircraft_type(aircraft_type: str) -> dict[str]:
@@ -346,14 +67,21 @@ def get_light_params_for_aircraft_type(aircraft_type: str) -> dict[str]:
 
 def main():
 
-    to_check_aircraft = "PC12"
-    result = get_light_params_for_aircraft_type(to_check_aircraft)
+    # aircrafts_definitions = "aircrafts.json"
+    # try:
+    #     with open(aircrafts_definitions, "r") as fp:
+    #         aircrafts = json.load(fp)
+    #         print("I have aircrafts!!")
+    # except FileNotFoundError:
+    #     logging.info(f"File {aircrafts_definitions} not found!")
 
-    print(f"Light params for {to_check_aircraft} are: {result}")
-
-    print(json.dumps(aircrafts))
+    # print(json.dumps(aircrafts))
+    # with open("aircrafts.json", "w") as fp:
+    #     fp.write(json.dumps(aircrafts, indent=4))
 
     print(json.dumps(light_params))
+    with open("light_params.json", "w") as fp:
+        fp.write(json.dumps(light_params, indent=4))
 
 
 if __name__ == "__main__":
