890bff22836ed71a3cc48270080f869aea3aeb15
DevFrog <froggi@xs4all.nl>
2024-03-15 01:27:53 +0100

    Slightly updated license text

--- lights_updater.py
+++ lights_updater.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2024  Richard Muller
+Copyright (C) 2024  Richard J.M. Muller / Froggi
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
