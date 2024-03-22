ead3d5c754060ff1149ba6332eb9454bd19b2070
DevFrog <froggi@xs4all.nl>
2024-03-22 15:21:41 +0100

    New time benchmark(s) added

--- decorators/time_benchmark.py
+++ decorators/time_benchmark.py
@@ -1,20 +1,23 @@
 from typing import Callable, Any
 from functools import wraps
 from time import perf_counter
+import logging
+
+log = logging.getLogger("benchmark")
 
 
 def time_benchmark(func: Callable[..., Any]) -> Any:
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any):
         start_time = perf_counter()
-        print(f"Starting {func.__name__}")
+        log.info(f"Starting {func.__name__}")
         result = func(*args, **kwargs)
-        print(f"Finished {func.__name__}")
+        log.info(f"Finished {func.__name__}")
         end_time = perf_counter()
 
         duration = end_time - start_time
 
-        print(f"It took {duration:.2f} seconds to complete {func.__name__}")
+        log.info(f"It took {duration:.2f} seconds to complete {func.__name__}")
         return result
 
     return wrapper
