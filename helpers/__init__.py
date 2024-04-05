98821a1b9733b404d8bef4ebce8a581a9dc7917e
DevFrog <froggi@xs4all.nl>
2024-04-05 18:01:00 +0200

    Added new imports

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,6 +2,9 @@ from .helpers import make_backup
 from .helpers import recover_from_backup
 from .helpers import delete_backups
 from .helpers import remove_xpmp2_files
+from .helpers import get_list_of_files
 
 from .aircraft_light_params import get_light_params_for_aircraft_type
 from .aircraft_helpers import get_aircraft_objects_from_xsb_file
+
+from .custom_exceptions import NoFilesFoundError
