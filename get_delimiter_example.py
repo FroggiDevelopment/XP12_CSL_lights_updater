146a215d50fd7e949e4d7ce5d152663a5f0c7cf4
Richard Muller <tfd0207@freedom.nl>
2024-04-15 15:59:03 +0200

    New way of getting the xsb delimiter

---
+++ get_delimiter_example.py
@@ -0,0 +1,12 @@
+string="hello/there/mister"
+string2="Hello:there:misses"
+string3="Hello"
+
+def get_delimiter(string: str) -> str:
+    if any ((match := delimiter) in string for delimiter in [":", "/"]):
+        return match
+    else:
+        return "None found!"
+    
+for test_string in [string, string2, string3]:
+    print (get_delimiter(test_string))
