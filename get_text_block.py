b526029480cc64b353e1ab5fe792474059127bf8
Richard Muller <tfd0207@freedom.nl>
2024-04-22 15:57:46 +0200

    Testfile for getting the taxillights within the landinglights anim...

---
+++ get_text_block.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import re
+
+start_delimiter: str = "ANIM_show 0.500000 2.000000 libxplanemp/controls/landing_lites_on"
+end_delimiter: str = "ANIM_end"
+
+file_to_open: Path = Path("CSL/BB_Airbus/A306/A306_AAW.obj")
+
+print(type(file_to_open))
+
+with open(file_to_open, "r") as file:
+    file_content = file.read()
+
+result = re.findall('(?s)ANIM_hide -1.000000 0.400000 libxplanemp/controls/landing_lites_on(.+?)ANIM_end', file_content)
+
+print(result)
+
+print(len(result))
+
+
+
+
