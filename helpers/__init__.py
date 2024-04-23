b75fff6e0e859096bcee4bb9723bcc477e156c2c
DevFrog <froggi@xs4all.nl>
2024-04-23 22:55:26 +0200

    Added new import

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -6,5 +6,6 @@ from .helpers import get_list_of_files
 
 from .aircraft_light_params import get_light_params_for_aircraft_type
 from .aircraft_helpers import get_aircraft_objects_from_xsb_file
+from .aircraft_helpers import fix_taxilights_dataref
 
 from .custom_exceptions import NoFilesFoundError
