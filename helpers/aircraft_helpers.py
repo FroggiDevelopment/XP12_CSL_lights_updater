9c63da2a5834b1d6853c9b53ab028c922e4e6817
DevFrog <froggi@xs4all.nl>
2024-04-23 22:56:17 +0200

    Added fix taxilights_dataref function to fix the wrong dataref in some objects

--- helpers/aircraft_helpers.py
+++ helpers/aircraft_helpers.py
@@ -16,6 +16,7 @@ Copyright (C) 2024  Richard J.M. Muller / Froggi
 """
 
 import sys
+import re
 import logging
 from pathlib import Path
 
@@ -53,7 +54,7 @@ def get_aircraft_objects_from_xsb_file(searchpath: str) -> list[Path]:
 
         try:
             with open(xsb_file, "r") as xsb_aircraft_file:
-                for line_num, line in enumerate(xsb_aircraft_file, start = 1):
+                for line_num, line in enumerate(xsb_aircraft_file, start=1):
                     # As some CSL aircraft are made with more then one object, also not recommended,
                     # some of those in xsb_aircraft.txt files have to be ignored!
                     if any(
@@ -71,7 +72,9 @@ def get_aircraft_objects_from_xsb_file(searchpath: str) -> list[Path]:
                     ):
                         continue
 
-                    if not line.startswith("OBJ8 "):  # Exclude the lines which have no light params
+                    if not line.startswith(
+                        "OBJ8 "
+                    ):  # Exclude the lines which have no light params
                         continue
 
                     aircraft_file_path_info: str = line.split()[3]
@@ -91,24 +94,55 @@ def get_aircraft_objects_from_xsb_file(searchpath: str) -> list[Path]:
                         aircraft_file_path_info.split(_separator)
                     )
                     if number_of_path_params == 3:
-                        _, relative_path, object_name = aircraft_file_path_info.split(_separator)
+                        _, relative_path, object_name = aircraft_file_path_info.split(
+                            _separator
+                        )
                     elif number_of_path_params == 2:
-                        relative_path, object_name = aircraft_file_path_info.split(_separator)
+                        relative_path, object_name = aircraft_file_path_info.split(
+                            _separator
+                        )
                     else:
                         log.warning(
                             f"Found not enough path parameters in {aircraft_file_path_info}! xsb_aircraft.txt file is not valid!"
                         )
                         sys.exit()
 
-                    object_path = Path(parentdir, relative_path, object_name.rstrip("\n"))
+                    object_path = Path(
+                        parentdir, relative_path, object_name.rstrip("\n")
+                    )
 
                     if object_path.exists() is False:
                         log.error(f"File {object_path} does not exist! Skipping!")
                         continue
 
-                    if ( object_path not in aircraft_object_files):
+                    if object_path not in aircraft_object_files:
                         aircraft_object_files.append(object_path)
 
         except FileNotFoundError as notfound:
             log.error(f"{xsb_file.name} not found! Skipping these!", notfound)
     return aircraft_object_files
+
+
+def fix_taxilights_dataref(object_content: str) -> str:
+    start_delimiter: str = (
+        "ANIM_show 0.500000 2.000000 libxplanemp/controls/landing_lites_on"
+    )
+    end_delimiter: str = "ANIM_end"
+    result = re.findall(
+        f"(?s)({start_delimiter})(.+?)({end_delimiter})", object_content
+    )
+
+    string_from_tuple = ""
+
+    for item in result:
+        if any("airplane_taxi" in value for value in item):
+            for row in item:
+                string_from_tuple += row
+
+    string_with_new_dataref = string_from_tuple.replace(
+        "landing_lites_on", "taxi_lites_on"
+    )
+    new_object_content = object_content.replace(
+        string_from_tuple, string_with_new_dataref
+    )
+    return new_object_content
