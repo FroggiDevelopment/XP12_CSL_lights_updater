660bdeaf955e7d033fdefdac17f7b686439de551
Richard Muller <tfd0207@freedom.nl>
2024-04-23 15:52:42 +0200

    It seems to work Wrong dataref for taxilights is fixed

--- get_text_block.py
+++ get_text_block.py
@@ -1,22 +1,40 @@
 from pathlib import Path
 import re
 
-start_delimiter: str = (
-    "ANIM_show 0.500000 2.000000 libxplanemp/controls/landing_lites_on"
-)
-end_delimiter: str = "ANIM_end"
+def get_original_object(object_path: Path) -> str:
+    with open(object_path, "r") as original_object:
+        return original_object.read()
+    
+def write_new_object(new_object_path: Path, content: str) -> None:
+     with open(new_object_path, "w") as file:
+        file.write(content)
 
-file_to_open: Path = Path("CSL/BB_Airbus/A306/A306_AAW_test.obj")
+def build_new_object(object_content: str) -> str:
+    start_delimiter: str = ("ANIM_show 0.500000 2.000000 libxplanemp/controls/landing_lites_on")
+    end_delimiter: str = "ANIM_end"
+    result = re.findall(f"(?s)({start_delimiter})(.+?)({end_delimiter})",object_content)
 
-print(type(file_to_open))
+    string_from_tuple = ""
 
-with open(file_to_open, "r") as file:
-    file_content = file.read()
+    for item in result:
+        if any( "airplane_taxi" in value for value in item):
+            for row in item:
+                string_from_tuple += row
 
-result = re.findall(
-    "(?s)(?<=ANIM_show 0.500000 2.000000 libxplanemp/controls/landing_lites_on)(.+?)ANIM_end",
-    file_content,
-)
+    string_with_new_dataref=string_from_tuple.replace("landing_lites_on", "taxi_lites_on")
+    new_object_content = object_content.replace(string_from_tuple, string_with_new_dataref)
+    return new_object_content
 
-for num, item in enumerate(result):
-    print(f"{num}. {item}")
+def repair_taxi_lights_dataref():
+    original_object_file: Path = Path("CSL/BB_GA/BE20/BE20_BGT_test.obj")
+    optimized_object_file: Path = Path("CSL/BB_GA/BE20/BE20_BGT_optimized.obj")
+    file_content = get_original_object(original_object_file)
+
+    new_object_content = build_new_object(file_content)
+    
+    with open(optimized_object_file, "w") as file:
+        file.write(new_object_content)
+    print("Done!")
+        
+if __name__ == "__main__":
+    repair_taxi_lights_dataref()
