183a3cd3b70a3a6deaeaf7833b944d0817c3af7a
Richard Muller <tfd0207@freedom.nl>
2024-07-15 14:59:28 +0200

    Rewritten to be conform PEP 484

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,11 +1,11 @@
-from .helpers import make_backup
-from .helpers import recover_from_backup
-from .helpers import delete_backups
-from .helpers import remove_xpmp2_files
-from .helpers import get_list_of_files
+from .helpers import make_backup as make_backup
+from .helpers import recover_from_backup as recover_from_backup
+from .helpers import delete_backups as delete_backups
+from .helpers import remove_xpmp2_files as remove_xpmp2_files
+from .helpers import get_list_of_files as get_list_of_files
 
-from .aircraft_light_params import get_light_params_for_aircraft_type
-from .aircraft_helpers import get_aircraft_objects_from_xsb_file
-from .aircraft_helpers import fix_lights_anomalies
+from .new_aircraft_light_params import get_light_params_for_aircraft_type as get_light_params_for_aircraft_type
+from .aircraft_helpers import get_aircraft_objects_from_xsb_file as get_aircraft_objects_from_xsb_file
+from .aircraft_helpers import fix_lights_anomalies as fix_lights_anomalies
 
-from .custom_exceptions import NoFilesFoundError
+from .custom_exceptions import NoFilesFoundError as NoFilesFoundError
