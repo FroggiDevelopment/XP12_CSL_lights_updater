852b0b2d4dc32dfed864914333a896014bf390eb
Richard Muller <tfd0207@freedom.nl>
2024-07-15 15:10:11 +0200

    Reverted back to "old" name for aircraft_light_params. The new file is now renamed.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,7 +4,7 @@ from .helpers import delete_backups as delete_backups
 from .helpers import remove_xpmp2_files as remove_xpmp2_files
 from .helpers import get_list_of_files as get_list_of_files
 
-from .new_aircraft_light_params import get_light_params_for_aircraft_type as get_light_params_for_aircraft_type
+from .aircraft_light_params import get_light_params_for_aircraft_type as get_light_params_for_aircraft_type
 from .aircraft_helpers import get_aircraft_objects_from_xsb_file as get_aircraft_objects_from_xsb_file
 from .aircraft_helpers import fix_lights_anomalies as fix_lights_anomalies
 
