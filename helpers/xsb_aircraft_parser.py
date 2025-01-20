739c5956da2e7df97e712740b1d96413b415e7ba
DevFrog <froggi@xs4all.nl>
2025-01-20 15:58:48 +0100

    Moved file to helpers.

--- helpers/xsb_aircraft_parser.py
+++ helpers/xsb_aircraft_parser.py
@@ -1,9 +1,12 @@
 import re
+import logging
 from pathlib import Path
 
-from helpers import get_list_of_files
+from .helpers import get_list_of_files
 
-PATH = "X-CSL/A320"
+log = logging.getLogger("xsb_aircraft_parser")
+
+PATH = "X-CSL"
 IGNORE_OBJECTS: list[str] = ["glass", "prop", "contrail", "fan", "rotor"]
 ICAO_IDENTIFIERS: list[str] = [
     "MATCHES",
@@ -12,6 +15,14 @@ ICAO_IDENTIFIERS: list[str] = [
     "LIVERY"]
 
 def get_xsb_files(searchpath: str):
+    """Gets a list of xsb_aircraft.txt files
+
+    Args:
+        searchpath (str): Path from where to search
+
+    Returns:
+        list[str]: The list of found xsb_aircraft.txt files
+    """
     return get_list_of_files(searchpath, "xsb_aircraft.txt")
 
 def get_path_part_from_line(line: str) -> str:
@@ -24,17 +35,28 @@ def get_path_part_from_line(line: str) -> str:
         str: string representation of the path
     """
     if not any((_separator := delimiter) in line for delimiter in [":", "/"]):
-        print(f"Could not find separator in {line} Can not create path to object file!")
-
-    pathinfo: str = line.split()[3]
-    # get rid of packagename, as this is ALWAYS the first part of the pathinfo!
-    path_only: str = pathinfo.split(_separator, 1)[1]
-    return(path_only)
+        log.error(f"Could not find separator in {line} Can not create path to object file!")
+        return "no sep error"
+    else:
+        pathinfo: str = line.split()[3]
+        # get rid of packagename, as this is ALWAYS the first part of the pathinfo!
+        path_only: str = pathinfo.split(_separator, 1)[1]
+        return(path_only)
 
 def parse_xsb_files(xsb_files: list[Path]):
+    """This function parses the xsb_aircraft.txt files and
+    creates a list of aircraft objects with icao identifiers and path info
+
+    Args:
+        xsb_files (list[Path]): _description_
+
+    Returns:
+        _type_: _description_
+    """
     aircraft_object_files: list[dict[str, str]] = []
+    unique_aircraft_objects: list[dict[str, str]] = []
     for xsb_file in xsb_files:
-        parentdir: Path = xsb_file.parent.absolute()
+        parentdir: str = str(xsb_file.parent.absolute())
         with open(xsb_file, "r") as xsb_aircraft_file:
             content: str = xsb_aircraft_file.read()
             aircraft_descriptions: list[str] = re.findall(
@@ -60,28 +82,23 @@ def parse_xsb_files(xsb_files: list[Path]):
                     if any (ignore_object in line.lower() for ignore_object in IGNORE_OBJECTS):
                         continue
                     
-                    #Get the parh to this aircraft object
+                    #Get the path to this aircraft object
                         
                         continue
                     path = get_path_part_from_line(line)
-                    full_path = str(parentdir) + "/" + path
+                    full_path = parentdir + "/" + path
                     aircraft_object["full_object_path"] = full_path
                     
                 aircraft_object_files.append(aircraft_object)
     
-        unique_aircraft_objects: list[dict[str, str]] = []
         [unique_aircraft_objects.append(val) for val in aircraft_object_files if val not in unique_aircraft_objects]
-
-        print(unique_aircraft_objects)
-        print(f"Aircrafts to convert in {str(parentdir)}: {len(unique_aircraft_objects)}")
+    return unique_aircraft_objects
 
                     
-def get_aircract_objects(searchpath: str):
+def get_aircraft_objects(searchpath: str):
     xsb_files = get_xsb_files(searchpath)
     list_of_aircraft_objects = parse_xsb_files(xsb_files)
     return list_of_aircraft_objects
                 
 if __name__ == "__main__":
-    aircraft_objects = get_aircract_objects(PATH)
-    # print(aircraft_objects)
-    # print(f"Found {len(aircraft_objects)} objects to convert!")
+    print("This is a library. Not usabel on its own!")
