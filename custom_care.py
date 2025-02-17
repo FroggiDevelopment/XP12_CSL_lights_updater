d805e4b53cd7d5d22e7e8ad35eb3bd9a94e98a5c
DevFrog <froggi@xs4all.nl>
2025-02-17 16:10:50 +0100

    Added some mofre logic. Light types are found now, params are loaded

--- custom_care.py
+++ custom_care.py
@@ -14,46 +14,78 @@ Copyright (C) 2025  Richard J.M. Muller / Froggi
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
+import re
+import sys
 from helpers import get_aircraft_objects_from_xsb_file
+from helpers.aircraft_light_params import get_light_params_for_aircraft_type
+from helpers import add_lateral_position_to_lights
 
-# from pathlib import Path
-
-def main():
-    path: str = "Custom"
-    good_aircraft_objects: list[dict[str, str]] = []
-    
-    light_to_search_for: list[str] = [
-        "airplane_landing",
-        "airplane_taxi",
-        "airplane_nav",
-        "airplane_strobe",
-        "airplane_beacon",
+WANTED_LIGHTS: list[str] = [
+    "airplane_landing",
+    "airplane_taxi",
+    "airplane_nav",
+    "airplane_nav_left",
+    "airplane_nav_right",
+    "airplane_nav_tail",
+    "airplane_strobe",
+    "airplane_strobe_tail",
+    "airplane_beacon",
+    "airplane_beacon_rotate",
+    "airplane_beacon_strobe",
         # Add more light types here as needed
     ]
 
-    aircraft_objects = get_aircraft_objects_from_xsb_file(path)
-    
+def get_aircraft_objects_with_lights(aircraft_objects: list[dict[str,str]]) -> list[dict[str,str]]:
+    good_aircraft_objects: list[dict[str, str]] = []
     for aircraft_object in aircraft_objects:
         with open(aircraft_object["full_object_path"], 'r') as aircraft_object_file:
             content: str = aircraft_object_file.read()
             
-            if any(light in content for light in light_to_search_for):
+            if any(light in content for light in WANTED_LIGHTS):
                 good_aircraft_objects.append(aircraft_object)
-                
-    print(f"Found {len(good_aircraft_objects)} objects with light(s) included")   
+    return good_aircraft_objects
+    
+
+def main():
+    path: str = "Custom/Diamond_DA62"
+
+    aircraft_objects = get_aircraft_objects_from_xsb_file(path)
+    unique_aircraft_lights: list[dict[str, str]] = []
+    
+    good_aircraft_objects = get_aircraft_objects_with_lights(aircraft_objects)            
+    print(f"Found {len(good_aircraft_objects)} objects with light(s) included")
+    
+    list_of_aircraft_with_lights: list[dict[str, str]] = []
     
     for good_aircraft_object in good_aircraft_objects:
+        light_params: dict[str, str] = get_light_params_for_aircraft_type(
+            str(good_aircraft_object["icao_type"])
+        )
+
         print("###################################################")
         print(f"{good_aircraft_object['full_object_path']}")
         print("###################################################")
-        with open(good_aircraft_object["full_object_path"]) as good_aircraft_object:
-            content_rows = good_aircraft_object.readlines()
+        with open(good_aircraft_object["full_object_path"]) as aircraft_object:
+            content_lines = aircraft_object.readlines()
             
-            for row in content_rows:
-                if "LIGHT_PARAM" in row or "LIGHT_NAMED" in row:
-                    if any(light in row for light in light_to_search_for):
-                        print(row.strip())    
+            for line in content_lines:
+                line = re.sub("#.* L", "", line)
+                line =  " ".join(line.split())
+                if "airplane_nav" in line or "airplane_strobe" in line:
+                    line = add_lateral_position_to_lights(line)
+                if "LIGHT_PARAM" in line or "LIGHT_NAMED" in line:
+                    for light in WANTED_LIGHTS:
+                        if light in line:
+                            coordinates = " ".join(line.split()[2:5])
+                            list_of_aircraft_with_lights.append({"light_type": light, "lightcoordinates": coordinates})
+
+        [unique_aircraft_lights.append(val) for val in list_of_aircraft_with_lights if val not in unique_aircraft_lights]
+        for aircraft_lights in unique_aircraft_lights:
+            print(aircraft_lights)
+                            
+                            
             
 
 if __name__ == '__main__':
     main()
+    sys.exit()
