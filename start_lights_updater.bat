a0b359747e42ef4a2b9ca9c4863ae6127d6721d5
DevFrog <froggi@xs4all.nl>
2025-02-24 22:21:07 +0100

    Advanced version with menu

--- start_lights_updater.bat
+++ start_lights_updater.bat
@@ -1,3 +1,50 @@
-ECHO ON
+ECHO OFF
+CLS
+:MENU
+SET do_this=4
+SET run_param=
+ECHO.
+ECHO ...................................................
+ECHO Default behavior on just pressing Enter is to exit!
+ECHO ...................................................
+ECHO 1. Run conversion
+ECHO 2. Undo conversion
+ECHO 3. Remove backups
+ECHO 4. Quits this
+ECHO ...................................................
+ECHO ...................................................
+set /p do_this="Please enter what I should do: "
+ECHO Youre choice: %do_this%
+
+IF %do_this% == 4 (
+CLS
+EXIT)
+
+IF %do_this% == 1 (
+ECHO Running conversion...
 CALL python lights_updater.py
+ECHO Press any key to continue....
 PAUSE
+CLS
+GOTO MENU
+)
+
+IF %do_this% == 2 (
+SET "run_param=-u"
+ECHO Running undo..........
+CALL python lights_updater.py %%run_param%%
+ECHO Press any key to continue....
+PAUSE
+CLS
+GOTO MENU
+)
+
+IF %do_this% == 3 (
+SET "run_param=-r"
+ECHO Removing backups.....
+CALL python lights_updater.py %%run_param%%
+ECHO Press any key to continue....
+PAUSE
+CLS
+GOTO MENU
+)
