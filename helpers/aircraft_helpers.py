7307314aeba8ccc6f502b066e9733fdd2b035681
DevFrog <froggi@xs4all.nl>
2025-05-30 16:56:38 +0200

    Added PropDisc and ZZZ to the IGNORE_OBJECTS

--- helpers/aircraft_helpers.py
+++ helpers/aircraft_helpers.py
@@ -31,7 +31,7 @@ from .custom_exceptions import NoFilesFoundError
 init_logging()
 log = logging.getLogger(__name__)
 
-IGNORE_OBJECTS: list[str] = ["glass", "prop",
+IGNORE_OBJECTS: list[str] = ["glass", "prop", "PropDisc", "ZZZ",
                              "Contrail", "fan", "rotor", "car", "BLUR", "EngineProp", "Glass"]
 ICAO_IDENTIFIERS: list[str] = [
     "MATCHES",
