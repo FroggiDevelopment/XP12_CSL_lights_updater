18a4565919bb9d6aa6b4f4bf8b9df777a0a6eaed
DevFrog <froggi@xs4all.nl>
2025-06-16 16:41:46 +0200

    Removed unused imports

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -19,14 +19,4 @@ from .aircraft_light_params import check_if_files_are_in_correct_json as check_i
 from .aircraft_helpers import get_aircraft_objects_from_xsb_file as get_aircraft_objects_from_xsb_file
 from .aircraft_helpers import get_list_of_animations as get_list_of_animations
 
-# from .light_manipulators import add_lateral_position_to_lights as add_lateral_position_to_lights
-# from .light_manipulators import reduce_spill_intensity as reduce_spill_intensity
-# from .light_manipulators import convert_airplane_landing_lights as convert_airplane_landing_lights
-# from .light_manipulators import convert_airplane_taxi_lights as convert_airplane_taxi_lights
-# from .light_manipulators import convert_airplane_nav_lights as convert_airplane_nav_lights
-# from .light_manipulators import convert_airplane_beacon_lights as convert_airplane_beacon_lights
-# from .light_manipulators import convert_airplane_flashing_beacon_lights as convert_airplane_flashing_beacon_lights
-# from .light_manipulators import convert_airplane_strobe_lights as convert_airplane_strobe_lights
-# from .light_manipulators import convert_airbus_strobe_lights as convert_airbus_strobe_lights
-
 from .custom_exceptions import NoFilesFoundError as NoFilesFoundError
