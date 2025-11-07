02f3430303e8a123156b22f494cb55c240df554e
DevFrog <froggi@xs4all.nl>
2025-11-07 20:17:27 +0100

    changed the way the function returns data. Now always a list is returned

--- helpers/aircraft_light_params.py
+++ helpers/aircraft_light_params.py
@@ -42,23 +42,21 @@ def check_if_files_are_in_correct_json() -> bool:
     return False
 
 
-def get_aircraft_with_flashing_beacons() -> list[str] | None:
+def get_aircraft_with_flashing_beacons() -> list[str]:
     try:
         with open(AIRCRAFT_DEFINITIONS, "r") as aircrafts_definitions:
             aircrafts = json.load(
                 aircrafts_definitions)
             if "aircraft_with_flashing_beacons" in aircrafts:
                 return aircrafts["aircraft_with_flashing_beacons"]
-            else:
-                return []
     except FileNotFoundError:
         log.error(f"Missing {AIRCRAFT_DEFINITIONS} file. Stopping now!")
-        paused_exit()
+
     except json.decoder.JSONDecodeError:
         log.error(
             f"{AIRCRAFT_DEFINITIONS} may be corrupted. Please check the file for consistency!"
         )
-        paused_exit()
+    return []
 
 
 def get_aircraft_categories() -> dict[str, str]:
