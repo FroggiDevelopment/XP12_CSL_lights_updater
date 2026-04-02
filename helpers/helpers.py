a8b318e8b40d6c619134151c1fe60ac02d7857e8
DevFrog <froggi@xs4all.nl>
2026-04-02 21:01:40 +0200

    Removed not really necessary delete step from move_processed_files_to_originals

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -234,17 +234,7 @@ def move_processed_files_to_originals(aircraft_objects: list[dict[str, str]], TE
         tmp_object_file: Path = Path(
             aircraft_object["full_object_path"]).with_suffix(TEMP_FILE_SUFFIX)
 
-        # try:
-        #     # First delete existing object. Added after Win11 does not like the below rename action anymore.
-        #     Path.unlink(
-        #         Path(aircraft_object["full_object_path"]), missing_ok=True)
-        # except PermissionError as err:
-        #     log.error(
-        #         f"Error on deleting {aircraft_object['full_object_path']}!", err)
-        #     continue
-
         try:
-            # Second copy new object
             tmp_object_file.replace(tmp_object_file.with_suffix(".obj"))
         except PermissionError as err:
             log.error(
